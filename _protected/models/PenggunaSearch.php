c6125ed01f1f7a6df209e4564133e877bc4aae65
Yusuf Yacob Ona Ola <yusuf.yacob@docotel.com>
2018-11-10 17:28:38 +0700

    update

--- _protected/models/PenggunaSearch.php
+++ _protected/models/PenggunaSearch.php
@@ -47,6 +47,11 @@ class PenggunaSearch extends Pengguna
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => [
+                'defaultOrder' => [
+                    'created_at' => SORT_DESC
+                ]
+            ]
         ]);
 
         $this->load($params);
